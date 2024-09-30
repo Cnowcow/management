a30abaa95f0d867ae5b86ab724a3e4d6ca40fcb7
nowcow <hhjnn92@icloud.com>
2024-09-30 16:41:48 +0900

    컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from "./components/Customer";
+import customer from "./components/Customer";
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'http://hhjnn92.synology.me/marketFile/default.png',
+    'name' : '최현우',
+    'birthday' : '920514',
+    'gender' : '남자',
+    'job' : '개발자'
+  },
+  {
+    'id' : 2,
+    'image' : 'http://hhjnn92.synology.me/marketFile/default.png',
+    'name' : '홍길동',
+    'birthday' : '920000',
+    'gender' : '남자',
+    'job' : '개발자'
+  },
+  {
+    'id' : 3,
+    'image' : 'http://hhjnn92.synology.me/marketFile/default.png',
+    'name' : '임꺽정',
+    'birthday' : '900000',
+    'gender' : '남자',
+    'job' : '개발자'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-    </div>
+      <div>
+        {
+          customers.map(c => {
+            return (
+                <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+            );
+          })
+        }
+      </div>
   );
 }
 
