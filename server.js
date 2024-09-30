3f18410dcdab9109daaac62e4c7b5b2e26136172
nowcow <hhjnn92@icloud.com>
2024-09-30 23:56:08 +0900

    데이터베이스 세팅 및 연동

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+
 app.get('/api/customers', (req,res) => {
-   res.send([
-       {
-           'id': 1,
-           'image': 'http://hhjnn92.synology.me/marketFile/default2.jpg',
-           'name': '최현우',
-           'birthday': '920514',
-           'gender': '남자',
-           'job': '개발자'
-       },
-       {
-           'id': 2,
-           'image': 'http://hhjnn92.synology.me/marketFile/default2.jpg',
-           'name': '홍길동',
-           'birthday': '920000',
-           'gender': '남자',
-           'job': '개발자'
-       },
-       {
-           'id': 3,
-           'image': 'http://hhjnn92.synology.me/marketFile/default2.jpg',
-           'name': '임꺽정',
-           'birthday': '900000',
-           'gender': '남자',
-           'job': '개발자'
-       }
-   ]);
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
