810185e278d8ef911b3a365e06e320155416d471
nowcow <hhjnn92@icloud.com>
2024-10-02 00:54:53 +0900

    AppBar 추가, 웹폰트 적용, 모든 컴포넌트 클래스형으로 전환

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Table, TableBody, TableHead, Paper, TableRow, TableCell, CircularProgress } from "@mui/material";
+import { makeStyles } from '@mui/styles';
 import Customer from "./components/Customer";
 import CustomerAdd from "./components/CustomerAdd";
-import { makeStyles } from '@mui/styles';  // @mui/styles로 수정
-import { createTheme, ThemeProvider } from '@mui/material/styles';  // @mui/material/styles는 ThemeProvider를 위해 유지
-import { Table, TableBody, TableHead, Paper } from "@mui/material";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import {CircularProgress} from "@mui/material";
+
+// AppBar 및 검색 관련 컴포넌트
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
 
 // 테마 생성
 const theme = createTheme({
@@ -16,98 +67,141 @@ const theme = createTheme({
 const useStyles = makeStyles(() => ({
     root: {
         width: '100%',
-        marginTop: theme.spacing(3),
-        overflowX: "auto"
-    },
-    table: {
         minWidth: 1080
+    },
+    paper:{
+        marginLeft: 18,
+        marginRight: 18,
     },
     progress: {
         margin: theme.spacing(2)
+    },
+    tableHead:{
+      fontSize: '1.0rem'
     }
 }));
 
-    /*
-        1. constructor()
-        2. componentWillMount()
-        3. render()
-        4. componentDidMount()
-        props or state => shouldComponentUpdate()
-    */
+/*
+    1. constructor()
+    2. componentWillMount()
+    3. render()
+    4. componentDidMount()
+    props or state => shouldComponentUpdate()
+*/
 
-function App() {
-    const [customers, setCustomers] = useState([]);  // 상태 초기화
-    const [completed, setCompleted] = useState(0);   // 프로그레스 바 상태
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            customers: [],
+            completed: 0,
+        };
+    }
 
     // 상태 초기화 함수
-    const stateRefresh = () => {
-        setCustomers([]);
-        setCompleted(0);
-        callApi()
-            .then(res => setCustomers(res))  // 고객 데이터를 상태에 저장
-            .catch(err => console.log(err));
+    stateRefresh = () => {
+        this.setState({
+            customers: [],
+            completed: 0,
+        }, this.callApi);
     };
 
     // 프로그레스 바 상태 업데이트 함수
-    const progress = () => {
-        setCompleted(prev => (prev >= 100 ? 0 : prev + 1));
+    progress = () => {
+        this.setState(prevState => ({
+            completed: prevState.completed >= 100 ? 0 : prevState.completed + 1
+        }));
     };
 
-    useEffect(() => {
-        const timer = setInterval(progress, 20);
-        callApi()
-            .then(res => setCustomers(res))  // 고객 데이터를 상태에 저장
-            .catch(err => console.log(err));
-
-        return () => clearInterval(timer); // 컴포넌트 언마운트 시 타이머 정리
-    }, []);  // 컴포넌트가 마운트될 때만 호출
-
-    const callApi = async () => {
-        const response = await fetch('/api/customers'); // 절대 경로가 아닌 상대 경로로 변경
-        const body = await response.json();
-        return body;
+    async componentDidMount() {
+        this.timer = setInterval(this.progress, 20);
+        this.callApi();
     }
 
-    const classes = useStyles();
+    componentWillUnmount() {
+        clearInterval(this.timer); // 컴포넌트 언마운트 시 타이머 정리
+    }
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Paper className={classes.root}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>번호</TableCell>
-                            <TableCell>이미지</TableCell>
-                            <TableCell>이름</TableCell>
-                            <TableCell>생년월일</TableCell>
-                            <TableCell>성별</TableCell>
-                            <TableCell>직업</TableCell>
-                            <TableCell>설정</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {customers.length > 0 ? customers.map(c => (
-                            <Customer
-                                stateRefresh={stateRefresh}
-                                key={c.id}
-                                id={c.id}
-                                image={c.image}
-                                name={c.name}
-                                birthday={c.birthday}
-                                gender={c.gender}
-                                job={c.job}
-                            />
-                        )) : <TableRow>
+    callApi = async () => {
+        const response = await fetch('/api/customers'); // 절대 경로가 아닌 상대 경로로 변경
+        const body = await response.json();
+        this.setState({ customers: body }); // 고객 데이터를 상태에 저장
+    };
+
+    render() {
+        const { customers, completed } = this.state;
+        const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정",];
+        return (
+            <div style={{ width: '100%', minWidth: 1080 }}>
+            <ThemeProvider theme={theme}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <IconButton
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="open drawer"
+                                sx={{ mr: 2 }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                component="div"
+                                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                            >
+                                고객 관리 시스템
+                            </Typography>
+                            <Search>
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
+                                    placeholder="검색하기"
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </Search>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <div style={{marginTop: 15, marginBottom: 15, display: 'flex', justifyContent: 'center' }}>
+                    <CustomerAdd stateRefresh={this.stateRefresh} />
+                </div>
+                <Paper>
+                    <Table style={{ minWidth: 1080 }}>
+                        <TableHead>
+                            <TableRow>
+                                {cellList.map((c, index) => (
+                                    <TableCell sx={{ fontSize: '1.0rem' }} key={index}>{c}</TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {customers.length > 0 ? customers.map(c => (
+                                <Customer
+                                    stateRefresh={this.stateRefresh}
+                                    key={c.id}
+                                    id={c.id}
+                                    image={c.image}
+                                    name={c.name}
+                                    birthday={c.birthday}
+                                    gender={c.gender}
+                                    job={c.job}
+                                />
+                            )) : <TableRow>
                                 <TableCell colSpan={6} align={"center"}>
-                                    <CircularProgress className={classes.progress} variant={"determinate"} value={completed} />
+                                    <CircularProgress variant={"determinate"} value={completed} />
                                 </TableCell>
                             </TableRow>}
-                    </TableBody>
-                </Table>
-            </Paper>
-            <CustomerAdd stateRefresh={stateRefresh}/>
-        </ThemeProvider>
-    );
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </ThemeProvider>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,19 +1,61 @@
 import React from "react";
+import { Dialog, DialogActions, DialogTitle, DialogContent, Button, Typography } from "@mui/material";
 
-function CustomerDelete({ id, stateRefresh }) {
+class CustomerDelete extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false // 다이얼로그 열림 여부를 관리하는 상태
+        };
+    }
 
-    const deleteCustomer = (id) => {
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        }); // 다이얼로그를 열기 위한 상태 업데이트
+    };
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        }); // 다이얼로그를 닫기 위한 상태 업데이트
+    };
+
+    deleteCustomer = (id) => {
         const url = `/api/customers/${id}`;
         fetch(url, {
             method: 'DELETE',
         })
-            .then(() => stateRefresh()); // API 호출 후 stateRefresh 실행
+            .then(() => {
+                this.props.stateRefresh(); // API 호출 후 부모 컴포넌트의 상태를 갱신
+                this.handleClose(); // 삭제 후 다이얼로그 닫기
+            })
+            .catch(err => console.log(err)); // 오류 처리
     };
 
-    return (
-        <button onClick={() => deleteCustomer(id)}>삭제</button>
-    );
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>
+                    삭제
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle onClose={this.handleClose}>
+                        삭제 경고
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            선택한 고객 정보가 삭제됩니다.
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={() => this.deleteCustomer(this.props.id)}>삭제</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
 }
-
 
 export default CustomerDelete;
